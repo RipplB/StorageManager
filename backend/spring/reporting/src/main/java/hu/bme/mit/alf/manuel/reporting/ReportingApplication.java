2f812892b2ced6380c3911343f9083a223788993
arionpap4 <arionpap4@gmail.com>
2023-05-21 20:25:15 +0200

    EmailService class created

--- backend/spring/reporting/src/main/java/hu/bme/mit/alf/manuel/reporting/ReportingApplication.java
+++ backend/spring/reporting/src/main/java/hu/bme/mit/alf/manuel/reporting/ReportingApplication.java
@@ -2,8 +2,14 @@ package hu.bme.mit.alf.manuel.reporting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("hu.bme.mit.alf.manuel.entityservice")
+@EntityScan("hu.bme.mit.alf.manuel.entityservice")
 @SpringBootApplication
+@ComponentScan("hu.bme.mit.alf.manuel")
 public class ReportingApplication {
 
 	public static void main(String[] args) {
