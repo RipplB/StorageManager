90951ea7423f26629eb9b90ca81c106cc600759a
RipplB <balrip.play@gmail.com>
2023-05-25 17:56:16 +0200

    Make build files ready for jenkins

--- backend/spring/reporting/build.gradle.kts
+++ backend/spring/reporting/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "hu.bme.mit.alf.manuel"
-version = "0.0.1-SNAPSHOT"
+version = findProperty("commonVersion")!!
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
